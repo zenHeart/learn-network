3128c0bec5938f8f1c9a820e58c7d01026e8ba72
zenheart <zenheart_register@163.com>
2021-01-05 19:48:27 +0800

    fix: 修复 half close 问题

--- protocol/tcp/server-half-close.js
+++ protocol/tcp/server-half-close.js
@@ -1,8 +1,11 @@
 const net= require('net')
+const { setImmediate, setInterval } = require('timers')
 const { Socket } = net
 
 function creatClient() {
-  const client = new Socket()
+  const client = new Socket({
+    allowHalfOpen: true
+  })
 
   client.connect({
     port: '10001'
@@ -14,13 +17,23 @@ function creatClient() {
       console.log('close half')
     })
   })
+  client.on('data', function(data) {
+    console.log('receive:' , data)
+    client.write('client send', data)
+  })
 }
 
 
-const server = net.createServer((socket) => {
-  console.log('connected')
-  socket.on('close' , function() {
-    console.log('socket close')
+const server = net.createServer({
+  allowHalfOpen: true
+}, (socket) => {
+  console.log(`client connected`)
+  socket.on('end' , function() {
+    const data = Array.from({
+      length: 1e3 + 1
+    }).join('1')
+    socket.write(data)
+    console.log('write data', data)
   })
   // socket.end('goodbye\n');
 }).on('error', (err) => {

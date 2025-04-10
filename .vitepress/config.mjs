10cb362c95d208b4e5e41fe65c4203c35614d4dd
zenheart <zenheart_register@163.com>
2025-04-10 15:31:11 +0800

    fix: 添加草稿说明网络手册需要包含的内容

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,5 +1,6 @@
-import { withMermaid } from "vitepress-plugin-mermaid"
-const isDev = process.env.npm_lifecycle_script.includes('dev')
+import { withMermaid } from "vitepress-plugin-mermaid";
+import checkbox from "markdown-it-checkbox";
+const isDev = process.env.npm_lifecycle_script.includes("dev");
 
 // https://vitepress.dev/reference/site-config
 export default withMermaid({
@@ -8,26 +9,30 @@ export default withMermaid({
   base: isDev ? "/" : "/learn-network/",
   themeConfig: {
     outline: [2, 3],
-    lastUpdated: 'Last Updated',
-    docsRepo: 'https://github.com/zenHeart/learn-network',
-    docsBranch: 'master',
+    lastUpdated: "Last Updated",
+    docsRepo: "https://github.com/zenHeart/learn-network",
+    docsBranch: "master",
     editLink: {
-      pattern: 'https://github.com/zenHeart/learn-network/edit/master/:path',
-      text: 'Edit this page on GitHub'
+      pattern: "https://github.com/zenHeart/learn-network/edit/master/:path",
+      text: "Edit this page on GitHub",
     },
     nav: [
-      { text: "指南", link: "/README", activeMatch: '/README|guide/' },
+      { text: "指南", link: "/README", activeMatch: "/README|guide/" },
       { text: "示例", link: "/examples/" },
       { text: "代办事项", link: "/todo/" },
     ],
     sidebar: {
-      '/guide/': createGuide(),
-      '/README': createGuide(),
-      '/examples/': createExamples(),
-    }
+      "/guide/": createGuide(),
+      "/README": createGuide(),
+      "/examples/": createExamples(),
+    },
+  },
+  markdown: {
+    config: (md) => {
+      md.use(checkbox);
+    },
   },
 });
-
 
 function createGuide() {
   return [
@@ -35,47 +40,49 @@ function createGuide() {
     {
       text: "基础",
       link: "/guide/01.fundamentals/index",
-      items: [
-        { text: "概念", link: "/guide/01.fundamentals/01.basic" },
-      ],
+      items: [{ text: "概念", link: "/guide/01.fundamentals/01.basic" }],
     },
     {
       text: "协议",
       link: "/guide/02.protocols/index",
       items: [
         {
-          text: '网络层',
-          items: [
-            { text: "IP", link: "/guide/02.protocols/02.ip" },
-          ]
+          text: "网络层",
+          items: [{ text: "IP", link: "/guide/02.protocols/02.ip" }],
         },
         {
-          text: '传输层',
+          text: "传输层",
           items: [
             { text: "ICMP", link: "/guide/02.protocols/03.00.icmp" },
             { text: "UDP", link: "/guide/02.protocols/03.01.udp" },
             { text: "TCP", link: "/guide/02.protocols/03.02.tcp" },
             { text: "SSL TLS", link: "/guide/02.protocols/03.03.ssl-tls" },
             { text: "Socket", link: "/guide/02.protocols/03.04.socket" },
-          ]
+          ],
         },
         {
-          text: '应用层',
+          text: "应用层",
           items: [
             { text: "DNS", link: "/guide/02.protocols/04.01.dns" },
             {
-              text: "HTTP", items: [
-                { text: "URI", link: "/guide/02.protocols/04.02.http/00.url-urn-uri" },
-                { text: "HTTP", link: "/guide/02.protocols/04.02.http/01.http" },
-              ]
+              text: "HTTP",
+              items: [
+                {
+                  text: "URI",
+                  link: "/guide/02.protocols/04.02.http/00.url-urn-uri",
+                },
+                {
+                  text: "HTTP",
+                  link: "/guide/02.protocols/04.02.http/01.http",
+                },
+              ],
             },
             { text: "WebSocket", link: "/guide/02.protocols/04.03.websocket" },
             { text: "MQTT", link: "/guide/02.protocols/04.04.mqtt" },
             { text: "CDN", link: "/guide/02.protocols/04.05.CDN" },
             { text: "OAuth", link: "/guide/02.protocols/04.06.OAuth" },
-          ]
+          ],
         },
-
       ],
     },
     { text: "浏览器", link: "/guide/03.browser/fetch" },
@@ -101,33 +108,30 @@ function createGuide() {
       ],
     },
     { text: "其他", link: "/guide/06.other/" },
-  ]
-
+  ];
 }
 
-
-
 function createExamples() {
-  return [{
-    text: "示例",
-    items: [
-      {
-        text: "概述",
-        link: "/examples/"
-      },
-      {
-        text: "http",
-        items: [
-          { text: "cros", link: "/examples/http/cros/README.md" },
-          { text: "cache", link: "/examples/http/cache/README.md" },
-        ],
-      },
-      {
-        text: "JSONP",
-        items: [
-          { text: "JSONP", link: "/examples/JSONP/README.md" },
-        ],
-      }
-    ],
-  }]
+  return [
+    {
+      text: "示例",
+      items: [
+        {
+          text: "概述",
+          link: "/examples/",
+        },
+        {
+          text: "http",
+          items: [
+            { text: "cros", link: "/examples/http/cros/README.md" },
+            { text: "cache", link: "/examples/http/cache/README.md" },
+          ],
+        },
+        {
+          text: "JSONP",
+          items: [{ text: "JSONP", link: "/examples/JSONP/README.md" }],
+        },
+      ],
+    },
+  ];
 }
